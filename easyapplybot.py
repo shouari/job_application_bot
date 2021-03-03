e7f3a35748202b31482b5278c9e63c66887b2c38
Salim Houari <salim.houari@gmail.com>
2021-03-03 17:13:52 +0300

    Extracting the job description text

--- easyapplybot.py
+++ easyapplybot.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import chromedriver_autoinstaller
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,6 +10,7 @@ from selenium.webdriver.common.keys import Keys
 import requests
 import json
 import time
+
 
 with open('data.json') as f:
     data = json.loads(f.read())
@@ -41,12 +43,20 @@ time.sleep(1)
 passwordElm.submit()
 
 browser.implicitly_wait(4)
-jobElem = browser.find_element_by_id('ember29')
-jobElem.click()
 
-# todo: find a way to minimize the message popup
-# closeMessage = browser.find_elements_by_xpath("//button[contains(@id,'msg-overlay-bubble-header__control')]")
-# closeMessage.click()
+try:
+    if browser.find_element_by_class_name('msg-overlay-list-bubble--is-minimized') is not None:
+        pass
+except NoSuchElementException:
+    try:
+        if browser.find_element_by_class_name('msg-overlay-bubble-header') is not None:
+            browser.find_element_by_class_name('msg-overlay-bubble-header').click()
+    except NoSuchElementException:
+        pass
+
+browser.get('https://www.linkedin.com/jobs/')
+
+
 job_titleElem = browser.find_element_by_xpath("//input[starts-with(@id,'jobs-search-box-keyword-id')]")
 time.sleep(2)
 job_titleElem.send_keys(job_title + Keys.TAB)
@@ -56,25 +66,29 @@ job_locationElem = browser.find_element_by_xpath("//input[starts-with(@id, 'jobs
 job_locationElem.send_keys(city + ',' + country + Keys.TAB + Keys.ENTER)
 
 
-def get_easy_apply_button():
-    try :
-        button = browser.find_elements_by_xpath(
-                    '//button[contains(@label, "LinkedIn Features")]/span[1]'
-                    )
-        button.click()
+# def get_easy_apply_button():
+#     try :
+#         button = browser.find_elements_by_xpath(
+#                     '//button[contains(@label, "LinkedIn Features")]/span[1]'
+#                     )
+#         button.click()
+#
+#         EasyApplyButton = button [0]
+#     except :
+#         EasyApplyButton = False
+#
+#     return EasyApplyButton
 
-        EasyApplyButton = button [0]
-    except :
-        EasyApplyButton = False
+def get_job_description( jobID):
 
-    return EasyApplyButton
-
-def get_job_page( jobID):
-
-    job = 'https://www.linkedin.com/jobs/view/'+ str(jobID)
+    job = 'https://www.linkedin.com/jobs/view/' + str(jobID)
     browser.get(job)
-    job_page = load_page(sleep=0.5)
-    return job_page
+
+    job_des = browser.find_element_by_xpath("//*[@id='job-details']")
+    job_description = job_des.get_attribute('innerHTML')
+    soup = BeautifulSoup(job_description.strip())
+
+    return soup.get_text()
 
 
 def load_page(sleep=1):
@@ -91,7 +105,7 @@ def load_page(sleep=1):
     page = BeautifulSoup(browser.page_source, features="xml")
     return page
 
-load_page(sleep=1)
+load_page()
 
 links = browser.find_elements_by_xpath('//div[@data-job-id]')
 # get job ID of each job link
@@ -109,15 +123,17 @@ IDs = set(IDs)
 
 print(IDs)
 count_job=0
-for i, jobID in enumerate(IDs):
-    count_job +=1
-    button = get_easy_apply_button()
-    if button is not False:
-        string_easy = "has Easy Apply "
-        print(jobID, string_easy)
-    else:
-        string_easy="Does not have easy apply"
-        print(jobID, string_easy)
+# for i, jobID in enumerate(IDs):
+#     count_job +=1
+#     button = get_easy_apply_button()
+#     if button is not False:
+#         string_easy = "has Easy Apply "
+#         print(jobID, string_easy)
+#     else:
+#         string_easy="Does not have easy apply"
+#         print(jobID, string_easy)
+for jobID in IDs:
+    print(get_job_description(jobID))
 
 
 
